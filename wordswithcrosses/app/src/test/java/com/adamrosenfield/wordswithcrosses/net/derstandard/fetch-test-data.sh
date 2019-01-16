099c33cdd7c6236c6a826e542ba832639f447b3b
Wolfgang Groiss <wolfgang.groiss@gmail.com>
2019-01-16 22:44:44 +0100

    derStandard: use https (for test at least)

--- wordswithcrosses/app/src/test/java/com/adamrosenfield/wordswithcrosses/net/derstandard/fetch-test-data.sh
+++ wordswithcrosses/app/src/test/java/com/adamrosenfield/wordswithcrosses/net/derstandard/fetch-test-data.sh
@@ -3,5 +3,5 @@
 # This script fetches test data for DerStandardParserTest.java
 # Copyright and License for this script: same as DerStandardParserTest.java, see there.  
 
-wget -O 7613-puzzle "http://derstandard.at/raetselapp/?id=7613&cd=&url=http%3a%2f%2fderstandard.at%2f1388650404174"
-wget -O 7613-solution --post-data=ExternalId=7613 http://derstandard.at/RaetselApp/Home/GetCrosswordResult
+wget -O 7613-puzzle "https://derstandard.at/raetselapp/?id=7613&cd=&url=http%3a%2f%2fderstandard.at%2f1388650404174"
+wget -O 7613-solution --post-data=ExternalId=7613 https://derstandard.at/RaetselApp/Home/GetCrosswordResult

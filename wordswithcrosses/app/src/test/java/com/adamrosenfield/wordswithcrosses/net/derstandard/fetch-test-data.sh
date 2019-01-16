802a7fc9b4100ab0845db9d66d860309e83c5aab
LittleLui <wolfgang.groiss@gmail.com>
2019-01-16 22:53:37 +0100

    Update fetch-test-data.sh

--- wordswithcrosses/app/src/test/java/com/adamrosenfield/wordswithcrosses/net/derstandard/fetch-test-data.sh
+++ wordswithcrosses/app/src/test/java/com/adamrosenfield/wordswithcrosses/net/derstandard/fetch-test-data.sh
@@ -4,4 +4,4 @@
 # Copyright and License for this script: same as DerStandardParserTest.java, see there.  
 
 wget -O 7613-puzzle "https://derstandard.at/raetselapp/?id=7613&cd=&url=http%3a%2f%2fderstandard.at%2f1388650404174"
-wget -O 7613-solution --post-data=ExternalId=7613 https://derstandard.at/RaetselApp/Home/GetCrosswordResult
+wget -O 7613-solution --post-data="{ExternalId:7613}" https://derstandard.at/RaetselApp/Home/GetCrosswordResult
